b15b45d59d1b547715d656d7acf2287d1e11ec71
Pradeep Kumar <pradeepkumar.anvi@gmail.com>
2018-08-05 16:22:51 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,19 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from:'Pradeep',
-        text:'Yup, that works for me.'
-    })    
-    // socket.emit('createEmail', {
-    //     to: 'jen@example.com',
-    //     text: 'Hey, This is Pradeep'
-    // });
-
-    // socket.on('createMessage', (message) => {
-    //     console.log('createMessage', message);
-    // })
 })
 
 socket.on('disconnect', function () {
