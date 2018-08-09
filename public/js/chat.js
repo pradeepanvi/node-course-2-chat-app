b22cc013a4a51955fdc03e73918f1c1f8d132829
pradeepanvi <pradeepkumar.anvi@gmail.com>
2018-08-09 17:35:28 +0530

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,11 +17,31 @@ function scrollToBottom(){
 }
 
 socket.on('connect', function () {
-    console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function(err){
+        if(err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No error');            
+        }
+
+    });
 })
 
 socket.on('disconnect', function () {
     console.log('Disconnnected from server');
+})
+
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+
+    users.forEach(function(user){
+        ol.append(jQuery('<li></li>').text(user));
+    })
+
+    jQuery('#users').html(ol);
 })
 
 socket.on('newMessage', function(message) {
@@ -46,13 +66,6 @@ socket.on('newLocationMessage', function(message) {
     });
     jQuery('#message').append(html);
     scrollToBottom();
-})
-
-socket.emit('createMessage', {
-    from: 'Frank',
-    text: 'Hi'
-}, function(data) {
-    console.log('Got it', data);
 })
 
 jQuery('#message-form').on('submit', function(e) {
